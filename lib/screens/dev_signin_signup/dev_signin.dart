831f4c6f46850af38b585a6d72179c81690f33db
Zhaozhan <nonox530042@gmail.com>
2024-06-07 03:17:55 -0400

    Added a demo sign-in and sign-up page for development.

---
+++ lib/screens/dev_signin_signup/dev_signin.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import '../../services/auth.dart';
+
+class DevSignInPage extends StatefulWidget {
+  @override
+  _DevSignInPageState createState() => _DevSignInPageState();
+}
+
+class _DevSignInPageState extends State<DevSignInPage> {
+  final AuthService _auth = AuthService();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async {
+                String email = _emailController.text;
+                String password = _passwordController.text;
+                dynamic result =
+                await _auth.signInWithEmailAndPassword(email, password);
+                if (result != null) {
+                  print('logged in');
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                    content: Text("Login Successful!!!"),
+                  ));
+                } else {
+                  print('Error logging in');
+                  print(result);
+                }
+              },
+              child: Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
