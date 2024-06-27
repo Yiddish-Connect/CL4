a85302f90fd2f43fcf3f3d18ef144d486d74843e
Zhaozhan <nonox530042@gmail.com>
2024-06-27 01:25:59 -0400

    Bug fix for Phone Login.

--- lib/screens/phone/phoneAuth.dart
+++ lib/screens/phone/phoneAuth.dart
@@ -8,6 +8,7 @@ import 'package:provider/provider.dart';
 import 'package:yiddishconnect/widgets/yd_multi_steps.dart';
 import 'dart:io';
 import '../../services/auth.dart';
+import '../../utils/formatters.dart';
 import '../../utils/helpers.dart';
 import '../dev_signin_signup/dev_home.dart';
 
@@ -36,23 +37,29 @@ class PhoneProvider extends ChangeNotifier {
   String? _verificationId; // Native only.
 
   String get phoneNumber => _phoneNumber;
+
   ConfirmationResult? get confirmationResult => _confirmationResult;
+
   PhoneAuthCredential? get phoneAuthCredential => _phoneAuthCredential;
+
   String? get verificationId => _verificationId;
 
-  set phoneNumber (String newPhoneNumber) {
+  set phoneNumber(String newPhoneNumber) {
     _phoneNumber = newPhoneNumber;
     notifyListeners();
   }
-  set confirmationResult (ConfirmationResult? newConfirmationResult) {
+
+  set confirmationResult(ConfirmationResult? newConfirmationResult) {
     _confirmationResult = newConfirmationResult;
     notifyListeners();
   }
-  set phoneAuthCredential (PhoneAuthCredential? newPhoneAuthCredential) {
+
+  set phoneAuthCredential(PhoneAuthCredential? newPhoneAuthCredential) {
     _phoneAuthCredential = newPhoneAuthCredential;
     notifyListeners();
   }
-  set verificationId (String? newVerificationId) {
+
+  set verificationId(String? newVerificationId) {
     _verificationId = newVerificationId;
     notifyListeners();
   }
@@ -62,7 +69,7 @@ class PhoneProvider extends ChangeNotifier {
 class _Step1 extends ActionWidget {
   final TextEditingController _phoneNumberController = TextEditingController();
   final AuthService _auth = AuthService();
-  
+
   _Step1({super.key, required super.action});
 
   @override
@@ -75,18 +82,24 @@ class _Step1 extends ActionWidget {
       child: Column(
         children: [
           Container(
-            padding: EdgeInsets.all(5),
-            child: TextField(
-              controller: _phoneNumberController,
-              keyboardType: TextInputType.phone,
-              decoration: InputDecoration(
-                labelText: 'Phone Number',
-                hintText: '(123) 456-7890',
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.phone),
-              ),
-              onChanged: (value) => Provider.of<PhoneProvider>(context, listen: false).phoneNumber = "+1 $value", // no need to rebuild a TextField
-            )
+              padding: EdgeInsets.all(5),
+              child: TextField(
+                  controller: _phoneNumberController,
+                  keyboardType: TextInputType.phone,
+                  inputFormatters: [PhoneNumberFormatter()],
+                  decoration: InputDecoration(
+                    labelText: 'Phone Number',
+                    hintText: '(123) 456-7890',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.phone),
+                  ),
+                  onChanged: (value) {
+                    final rawNumber = value.replaceAll(RegExp(r'\D'), ''); // Remove formatting for storing raw number
+                    Provider
+                        .of<PhoneProvider>(context, listen: false)
+                        .phoneNumber = "+1 $rawNumber"; // no need to rebuild a TextField
+                  }
+              )
           ),
           Container(
               padding: EdgeInsets.only(top: 40),
@@ -95,32 +108,50 @@ class _Step1 extends ActionWidget {
                 child: FractionallySizedBox(
                   widthFactor: 0.6,
                   child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(backgroundColor: Theme.of(context).colorScheme.primary, foregroundColor: Theme.of(context).colorScheme.onPrimary),
+                      style: ElevatedButton.styleFrom(backgroundColor: Theme
+                          .of(context)
+                          .colorScheme
+                          .primary, foregroundColor: Theme
+                          .of(context)
+                          .colorScheme
+                          .onPrimary),
                       onPressed: () async {
                         print("onPressed() called...");
                         try {
-                          String phoneNumber = context.read<PhoneProvider>().phoneNumber;
-                          // Might take some time in Android. Because Firebase will first try to do auto-complete first before codeAutoRetrievalTimeout() happens.
+                          String phoneNumber = context
+                              .read<PhoneProvider>()
+                              .phoneNumber;
                           toast(context, "SMS code sent to $phoneNumber");
-                          void onConfirmationResult (ConfirmationResult confirmationResult) {
-                            Provider.of<PhoneProvider>(context, listen: false).confirmationResult = confirmationResult;
+                          // Web only. Will be triggered when code successfully sent.
+                          void onConfirmationResult(ConfirmationResult confirmationResult) {
+                            print("onConfirmationResult() called...");
+                            Provider
+                                .of<PhoneProvider>(context, listen: false)
+                                .confirmationResult = confirmationResult;
+                            action();
                           }
-                          void onCodeSent (String verificationId) {
+                          // Native only. Will be triggered when code successfully sent.
+                          void onCodeSent(String verificationId) {
                             print("onCodeSent() called...");
                             if (!context.mounted) {
                               throw Exception("onCodeSent(): context.mounted is false!!");
                               return;
                             }
-                            Provider.of<PhoneProvider>(context, listen: false).verificationId = verificationId;
+                            Provider
+                                .of<PhoneProvider>(context, listen: false)
+                                .verificationId = verificationId;
                             action(); // go to next page
                           }
-                          void onAutoResolution (PhoneAuthCredential credential) {
-                            Provider.of<PhoneProvider>(context, listen: false).phoneAuthCredential = credential;
+                          // Android Native only. Will be triggered when code successfully auto resolved
+                          void onAutoResolution(PhoneAuthCredential credential) {
                             print("onAutoResolution() called...");
                             if (!context.mounted) {
-                              throw Exception("onCodeSent(): context.mounted is false!!");
+                              throw Exception("onAutoResolution(): context.mounted is false!!");
                               return;
                             }
+                            Provider
+                                .of<PhoneProvider>(context, listen: false)
+                                .phoneAuthCredential = credential;
                           }
                           await _auth.sendCodeToPhoneNumber(phoneNumber, onConfirmationResult, onCodeSent, onAutoResolution);
                         } catch (e) {
@@ -140,7 +171,7 @@ class _Step1 extends ActionWidget {
 
 // _Step2: Enter your SMS code
 class _Step2 extends ActionWidget {
-  final TextEditingController _codeController = TextEditingController();
+
   final AuthService _auth = AuthService();
 
   _Step2({super.key, required super.action});
@@ -148,69 +179,85 @@ class _Step2 extends ActionWidget {
   @override
   Widget build(BuildContext context) {
     print("_Step2 built...");
-    return Container(
-      padding: EdgeInsets.all(30),
-      constraints: BoxConstraints(maxWidth: 600),
-      alignment: Alignment.center,
-      child: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.all(5),
-            child: Consumer<PhoneProvider>(
-              builder: (context, phoneNumberProvider, child) {
-                return Text(
-                  "Enter OTP code we sent to ${phoneNumberProvider.phoneNumber}. This code will expire in 2 minutes",
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.titleLarge,
-                );
-              }
+    return Consumer<PhoneProvider>(
+        builder: (context, phoneNumberProvider, child) {
+          final TextEditingController codeController = TextEditingController(text: phoneNumberProvider.phoneAuthCredential != null ? phoneNumberProvider.phoneAuthCredential?.smsCode ?? "" : "");
+          return Container(
+            padding: EdgeInsets.all(30),
+            constraints: BoxConstraints(maxWidth: 600),
+            alignment: Alignment.center,
+            child: Column(
+              children: [
+                Container(
+                    padding: EdgeInsets.all(5),
+                    child: Text(
+                      "Enter the OTP code we sent to (${phoneNumberProvider.phoneNumber.substring(3, 6)})-${phoneNumberProvider.phoneNumber.substring(6, 9)}-${phoneNumberProvider.phoneNumber.substring(9, 13)}. The code expires in 5 minutes",
+                      textAlign: TextAlign.center,
+                      style: Theme
+                          .of(context)
+                          .textTheme
+                          .titleLarge,
+                    )
+                ),
+                Container(
+                    padding: EdgeInsets.all(5),
+                    child: TextField(
+                      controller: codeController,
+                      keyboardType: TextInputType.phone,
+                      decoration: InputDecoration(
+                        labelText: '6-digits code',
+                        hintText: "123456",
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.phone),
+                      ),
+                    )
+                ),
+                Container(
+                    padding: EdgeInsets.only(top: 40),
+                    child: SizedBox(
+                      height: 50,
+                      child: FractionallySizedBox(
+                        widthFactor: 0.6,
+                        child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(backgroundColor: Theme
+                                .of(context)
+                                .colorScheme
+                                .primary, foregroundColor: Theme
+                                .of(context)
+                                .colorScheme
+                                .onPrimary),
+                            onPressed: () async {
+                              try {
+                                String smsCode = codeController.text;
+                                User? user = await _auth.signInWithSMSCode(
+                                    smsCode: smsCode,
+                                    verificationId: context
+                                        .read<PhoneProvider>()
+                                        .verificationId,
+                                    phoneAuthCredential: context
+                                        .read<PhoneProvider>()
+                                        .phoneAuthCredential,
+                                    confirmationResult: context
+                                        .read<PhoneProvider>()
+                                        .confirmationResult
+                                );
+                                if (user != null) {
+                                  Navigator.push(context, MaterialPageRoute(builder: (context) => DevHome()));
+                                } else {
+                                  toast(context, "Something went wrong (null)");
+                                }
+                              } catch (e) {
+                                toast(context, e.toString());
+                              }
+                            },
+                            child: Text("Verify")),
+                      ),
+                    )
+                )
+              ],
             ),
-          ),
-          Container(
-              padding: EdgeInsets.all(5),
-              child: TextField(
-                controller: _codeController,
-                keyboardType: TextInputType.phone,
-                decoration: InputDecoration(
-                  labelText: '6-digits code',
-                  hintText: "123456",
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.phone),
-                ),
-              )
-          ),
-          Container(
-              padding: EdgeInsets.only(top: 40),
-              child: SizedBox(
-                height: 50,
-                child: FractionallySizedBox(
-                  widthFactor: 0.6,
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(backgroundColor: Theme.of(context).colorScheme.primary, foregroundColor: Theme.of(context).colorScheme.onPrimary),
-                      onPressed: () async {
-                        try {
-                          String smsCode = _codeController.text;
-                          User? user = await _auth.signInWithSMSCode(
-                            smsCode: smsCode,
-                            verificationId: context.read<PhoneProvider>().verificationId,
-                            phoneAuthCredential: context.read<PhoneProvider>().phoneAuthCredential,
-                            confirmationResult: context.read<PhoneProvider>().confirmationResult
-                          );
-                          if (user != null) {
-                            Navigator.push(context, MaterialPageRoute(builder: (context) => DevHome()));
-                          } else {
-                            toast(context, "Something went wrong (null)");
-                          }
-                        } catch (e) {
-                          toast(context, e.toString());
-                        }
-                      },
-                      child: Text("Verify")),
-                ),
-              )
-          )
-        ],
-      ),
+          );
+        }
     );
   }
 }
@@ -220,9 +267,11 @@ class _Step3 extends ActionWidget {
 
   @override
   Widget build(BuildContext context) {
-    print("_Step3 built");
-    return const Placeholder();
+  print("_Step3 built");
+  return const Placeholder();
   }
 }
+
+
 
 
