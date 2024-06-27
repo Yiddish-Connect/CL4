c14e5d6c99e1873a9edd83ecbbfb7baa42d2c596
Zhaozhan <nonox530042@gmail.com>
2024-06-26 22:36:06 -0400

    Added 'Resend verification Email' in Email Sign-up

--- lib/screens/email/emailSignUp.dart
+++ lib/screens/email/emailSignUp.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:yiddishconnect/utils/helpers.dart';
 import '../../services/auth.dart';
 import '../../widgets/yd_multi_steps.dart';
@@ -11,18 +12,32 @@ class EmailSignUpScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiSteps(
-      steps: [
-        OneStep(
-          title: "Step 1: Enter your Email and password",
-          builder: (callback) => _Step1(action: callback),
-        ),
-        OneStep(
-          title: "Step 2: Verify your Email address",
-          builder: (callback) => _Step2(action: callback),
-        ),
-      ],
+    return ChangeNotifierProvider(
+      create: (context) => EmailProvider(),
+      child: MultiSteps(
+        steps: [
+          OneStep(
+            title: "Step 1: Enter your Email and password",
+            builder: (callback) => _Step1(action: callback),
+          ),
+          OneStep(
+            title: "Step 2: Verify your Email address",
+            builder: (callback) => _Step2(action: callback),
+          ),
+        ],
+      ),
     );
+  }
+}
+
+class EmailProvider extends ChangeNotifier {
+  String _email = "";
+
+  String get email => _email;
+
+  set email(String newEmail) {
+    _email = newEmail;
+    notifyListeners();
   }
 }
 
@@ -71,11 +86,20 @@ class _Step1 extends ActionWidget {
               child: FractionallySizedBox(
                 widthFactor: 0.6,
                 child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(backgroundColor: Theme.of(context).colorScheme.primary, foregroundColor: Theme.of(context).colorScheme.onPrimary),
+                  style: ElevatedButton.styleFrom(backgroundColor: Theme
+                      .of(context)
+                      .colorScheme
+                      .primary, foregroundColor: Theme
+                      .of(context)
+                      .colorScheme
+                      .onPrimary),
                   onPressed: () async {
                     String email = _emailController.text;
                     String password = _passwordController.text;
                     try {
+                      Provider
+                          .of<EmailProvider>(context, listen: false)
+                          .email = email; // store the email in EmailProvider
                       User? user = await _auth.registerWithEmailAndPassword(email, password);
                       if (user != null) {
                         await user.sendEmailVerification();
@@ -86,7 +110,6 @@ class _Step1 extends ActionWidget {
                       }
                     } catch (e) {
                       toast(context, e.toString());
-                      action(); // This is for debug purpose. TODO: delete this
                     }
                   },
                   child: Text('Register'),
@@ -102,59 +125,83 @@ class _Step1 extends ActionWidget {
 
 // Step 2: Ask user to click verification link
 class _Step2 extends ActionWidget {
-  const _Step2({super.key, required super.action});
+  _Step2({super.key, required super.action});
+
+  final AuthService _auth = AuthService();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(30),
-      constraints: BoxConstraints(maxWidth: 600),
-      alignment: Alignment.center,
-      child: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.all(5),
-            child: Text(
-              "A verification Email has been sent to your address",
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-          ),
-          Container(
-              padding: EdgeInsets.all(5),
-              child: RichText(text: TextSpan(
-                  text: "Didn't receive the email?",
-                  style: Theme.of(context).textTheme.labelMedium,
-                  children: [
-                    TextSpan(
-                        text: "Resend",
-                        style: Theme.of(context).textTheme.labelMedium,
-                        recognizer: TapGestureRecognizer()..onTap = () {
-                          toast(context, "We have resent another verification email to ");
-                        }
-                    )
-                  ]
-              ))
-          ),
-          Container(
-              padding: EdgeInsets.only(top: 50),
-              child: SizedBox(
-                height: 50,
-                child: FractionallySizedBox(
-                  widthFactor: 0.6,
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(backgroundColor: Theme.of(context).colorScheme.primary, foregroundColor: Theme.of(context).colorScheme.onPrimary),
-                      onPressed: () {
-                        // TODO: Check verification
-                        toast(context, "TODO");
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => DevHome()));
-                      },
-                      child: Text("I have verified my Email")),
+    return Consumer<EmailProvider>(
+      builder: (context, emailProvider, child) {
+        return Container(
+          padding: EdgeInsets.all(30),
+          constraints: BoxConstraints(maxWidth: 600),
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.all(5),
+                child: Text(
+                  "A verification Email has been sent to ${emailProvider.email}",
+                  textAlign: TextAlign.center,
+                  style: Theme
+                      .of(context)
+                      .textTheme
+                      .titleLarge,
                 ),
-              )
-          )
-        ],
-      ),
+              ),
+              Container(
+                  padding: EdgeInsets.all(5),
+                  child: RichText(
+                      text: TextSpan(text: "Didn't receive the email?", style: Theme
+                          .of(context)
+                          .textTheme
+                          .labelMedium, children: [
+                        TextSpan(
+                            text: "Resend",
+                            style: Theme
+                                .of(context)
+                                .textTheme
+                                .labelMedium,
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () async {
+                                try {
+                                  await _auth.getUser()?.sendEmailVerification();
+                                  toast(context, "We have resent another verification email to ${emailProvider.email}");
+                                }
+                                catch (e) {
+                                  toast(context, e.toString());
+                                }
+                              })
+                      ]))),
+              Container(
+                  padding: EdgeInsets.only(top: 50),
+                  child: SizedBox(
+                    height: 50,
+                    child: FractionallySizedBox(
+                      widthFactor: 0.6,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(backgroundColor: Theme
+                              .of(context)
+                              .colorScheme
+                              .primary, foregroundColor: Theme
+                              .of(context)
+                              .colorScheme
+                              .onPrimary),
+                          onPressed: () {
+                            if (_auth.getUser() == null) {
+                              toast(context, "Verification Failed");
+                            } else {
+                              Navigator.push(context, MaterialPageRoute(builder: (context) => DevHome()));
+                            }
+                          },
+                          child: Text("I have verified my Email")),
+                    ),
+                  ))
+            ],
+          ),
+        );
+      },
     );
   }
 }
