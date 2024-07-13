c4b9efe37fcc718f4d71f15b5587484d3ab8297e
zhzh <nonox530042@gmail.com>
2024-07-13 19:16:08 -0400

    Adjusted the landing screen & auth screen layout by adding a Flexible() spacer

--- lib/screens/landing.dart
+++ lib/screens/landing.dart
@@ -19,143 +19,152 @@ class LandingScreen extends StatelessWidget {
         color: Theme.of(context).colorScheme.background,
         child: Padding(
           padding: const EdgeInsets.all(20),
-          child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-            Container(
-              width: 500 + 20,
-              height: 300 + 20,
-              // color: Colors.amber,
-              padding: EdgeInsets.all(10),
-              child: Center(
-                child: Stack(
-                  children: [
-                    // The animated curve
-                    Align(
-                      alignment: Alignment.center,
-                      child: AnimatedCurve()
-                    ),
-                    // The user avatar 1
-                    Positioned(
-                      top: 100,
-                      left: 40,
-                      child: Opacity(
-                        opacity: 0.8,
-                        child: CircleAvatar(
-                          backgroundImage: NetworkImage('https://picsum.photos/200'),
-                          radius: 50,
-                        ),
-                      )
-                    ),
-                    // The user avatar 2
-                    Positioned(
-                      top: 10,
-                      right: 40,
-                      child: Opacity(
-                        opacity: 0.8,
-                        child: CircleAvatar(
-                          backgroundImage: NetworkImage('https://picsum.photos/200'),
-                          radius: 50,
-                        ),
-                      )
-                    ),
-                    // "Intermediate"
-                    Positioned(
-                      top: 20,
-                      left: 60,
-                      child: Label(
-                        borderColor: Theme.of(context).colorScheme.primary,
-                        backgroundColor: Theme.of(context).colorScheme.background,
-                        opacity: 0.7,
-                        text: "Intermediate",
-                        height: 30,
-                        width: 100,
-                      )
-                      
-                    ),
-                    // "Proficient"
-                    Positioned(
-                      bottom: 120,
-                      right: 60,
-                      child: Label(
-                        borderColor: Theme.of(context).colorScheme.secondary,
-                        backgroundColor: Theme.of(context).colorScheme.background,
-                        opacity: 0.7,
-                        text: "Proficient",
-                        height: 30,
-                        width: 100,
-                      )
-                    ),
-                  ],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Flexible(
+                flex: 1,
+                child: SizedBox(
+                  height: 50, // Set the initial minimum height here
                 ),
               ),
-            ),
-            Container(
-              // The text
-              margin: const EdgeInsets.all(20.0),
-              constraints: BoxConstraints(minHeight: 100),
-              child: Center(
-                  child: Text(
-                "Make friends with people like you",
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.headlineLarge,
-              )),
-            ),
-            Container(
-              // The 2 buttons
-              margin: const EdgeInsets.all(20.0),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: SizedBox(
-                      width: double.infinity,
-                      height: 50,
-                      child: FractionallySizedBox(
-                        widthFactor: 0.6,
-                        child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(backgroundColor: Theme.of(context).colorScheme.primary, foregroundColor: Theme.of(context).colorScheme.onPrimary),
-                            // Continue => Anonymous mode (TODO)
-                            onPressed: () async {
-                              try {
-                                User? user = await AuthService().signInAnonymously();
-                                if (user != null) {
-                                  context.go("/home");
-                                } else {
-                                  toast(context, "Something went wrong (null)");
-                                }
-                              } catch (e) {
-                                toast(context, e.toString());
-                              }
-                            },
-                            child: Text("Continue")
-                            // Don't need to specify the style here.
-                            // The default style here is inherited from ElevatedButton, which will automatically looks for labelMedium
-                            ),
+              Container(
+                width: 500 + 20,
+                height: 300 + 20,
+                // color: Colors.amber,
+                padding: EdgeInsets.all(10),
+                child: Center(
+                  child: Stack(
+                    children: [
+                      // The animated curve
+                      Align(
+                        alignment: Alignment.center,
+                        child: AnimatedCurve()
                       ),
-                    ),
+                      // The user avatar 1
+                      Positioned(
+                        top: 100,
+                        left: 40,
+                        child: Opacity(
+                          opacity: 0.8,
+                          child: CircleAvatar(
+                            backgroundImage: NetworkImage('https://picsum.photos/200'),
+                            radius: 50,
+                          ),
+                        )
+                      ),
+                      // The user avatar 2
+                      Positioned(
+                        top: 10,
+                        right: 40,
+                        child: Opacity(
+                          opacity: 0.8,
+                          child: CircleAvatar(
+                            backgroundImage: NetworkImage('https://picsum.photos/200'),
+                            radius: 50,
+                          ),
+                        )
+                      ),
+                      // "Intermediate"
+                      Positioned(
+                        top: 20,
+                        left: 60,
+                        child: Label(
+                          borderColor: Theme.of(context).colorScheme.primary,
+                          backgroundColor: Theme.of(context).colorScheme.background,
+                          opacity: 0.7,
+                          text: "Intermediate",
+                          height: 30,
+                          width: 100,
+                        )
+
+                      ),
+                      // "Proficient"
+                      Positioned(
+                        bottom: 120,
+                        right: 60,
+                        child: Label(
+                          borderColor: Theme.of(context).colorScheme.secondary,
+                          backgroundColor: Theme.of(context).colorScheme.background,
+                          opacity: 0.7,
+                          text: "Proficient",
+                          height: 30,
+                          width: 100,
+                        )
+                      ),
+                    ],
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: SizedBox(
-                      width: double.infinity,
-                      height: 50,
-                      child: FractionallySizedBox(
-                        widthFactor: 0.6,
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(backgroundColor: Theme.of(context).colorScheme.surface, foregroundColor: Theme.of(context).colorScheme.onSurface),
-                          // Sign in => SignInPage
-                          onPressed: () {
-                            context.go("/auth");
-                          },
-                          child: Text("Login"),
-                          // Don't need to specify the style here.
-                          // The default style here is inherited from ElevatedButton, which will automatically looks for labelMedium
+                ),
+              ),
+              Container(
+                // The text
+                margin: const EdgeInsets.all(20.0),
+                constraints: BoxConstraints(minHeight: 100),
+                child: Center(
+                    child: Text(
+                  "Make friends with people like you",
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.headlineLarge,
+                )),
+              ),
+              Container(
+                // The 2 buttons
+                margin: const EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: SizedBox(
+                        width: double.infinity,
+                        height: 50,
+                        child: FractionallySizedBox(
+                          widthFactor: 0.6,
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(backgroundColor: Theme.of(context).colorScheme.primary, foregroundColor: Theme.of(context).colorScheme.onPrimary),
+                              // Continue => Anonymous mode (TODO)
+                              onPressed: () async {
+                                try {
+                                  User? user = await AuthService().signInAnonymously();
+                                  if (user != null) {
+                                    context.go("/home");
+                                  } else {
+                                    toast(context, "Something went wrong (null)");
+                                  }
+                                } catch (e) {
+                                  toast(context, e.toString());
+                                }
+                              },
+                              child: Text("Continue")
+                              // Don't need to specify the style here.
+                              // The default style here is inherited from ElevatedButton, which will automatically looks for labelMedium
+                              ),
                         ),
                       ),
                     ),
-                  )
-                ],
-              ),
-            )
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: SizedBox(
+                        width: double.infinity,
+                        height: 50,
+                        child: FractionallySizedBox(
+                          widthFactor: 0.6,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(backgroundColor: Theme.of(context).colorScheme.surface, foregroundColor: Theme.of(context).colorScheme.onSurface),
+                            // Sign in => SignInPage
+                            onPressed: () {
+                              context.go("/auth");
+                            },
+                            child: Text("Login"),
+                            // Don't need to specify the style here.
+                            // The default style here is inherited from ElevatedButton, which will automatically looks for labelMedium
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
           ]),
         ),
       ),
