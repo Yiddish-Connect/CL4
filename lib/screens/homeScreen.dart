d9eb1cbe0a0ed0d237ad18707732716cc78be674
zhzh <nonox530042@gmail.com>
2024-07-14 16:25:20 -0400

    Created the skeleton of the homeScreen using BottomNavigationBar + IndexedStack

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:yiddishconnect/services/auth.dart';
 import 'package:yiddishconnect/utils/helpers.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,8 +11,8 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int _index = 0; // Index of the child currently visible
-  List<Widget> _screens = [
-    HomeScreen(),
+  List<Widget> _pages = [
+    HomePage(),
     TestWidgetOne(),
     TestWidgetTwo(),
     TestWidgetThree()
@@ -22,13 +21,57 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("TODO: a title"),
+      ),
+      body: IndexedStack(
+        index: _index,
+        children: _pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.one_k),
+            label: "One",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.two_k),
+            label: "Two",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.three_k),
+            label: "Three",
+          ),
+        ],
+        currentIndex: _index,
+        onTap: (int selectedIndex) {
+          setState(() {
+            _index = selectedIndex;
+          });
+        },
+        backgroundColor: Theme.of(context).colorScheme.background,
+        selectedItemColor: Theme.of(context).colorScheme.primary,
+        unselectedItemColor: Theme.of(context).colorScheme.secondary,
+      ),
+    );
   }
 }
 
 
 
-
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Homeeeee'),
+    );
+  }
+}
 
 class TestWidgetOne extends StatelessWidget {
   @override
