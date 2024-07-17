1385cec3f49e446c004c383362c9f8b7e25b5a52
zhzh <nonox530042@gmail.com>
2024-07-17 19:53:44 -0400

    Added the filter value to MatchPageProvider, which is a provider of homeScreen

--- lib/screens/match/matchPageProvider.dart
+++ lib/screens/match/matchPageProvider.dart
@@ -2,6 +2,53 @@ import 'dart:collection';
 import 'package:flutter/material.dart';
 import '../../models/filter.dart';
 
-class MatchPageProvider with ChangeNotifier {
+class MatchPageProvider extends ChangeNotifier {
+  List<PracticeOption> _practiceOptionsSelection = [];
+  List<YiddishProficiency> _yiddishProficiencySelection = [];
+  int _minDistance = 0;
+  int _maxDistance = 1000;
+  int _minAge = 0;
+  int _maxAge = 100;
 
+  List<PracticeOption> get practiceOptionsSelection => _practiceOptionsSelection;
+
+  set practiceOptionsSelection(List<PracticeOption> value) {
+    _practiceOptionsSelection = value;
+    notifyListeners();
+  }
+
+  int get maxAge => _maxAge;
+
+  set maxAge(int value) {
+    _maxAge = value;
+    notifyListeners();
+  }
+
+  int get minAge => _minAge;
+
+  set minAge(int value) {
+    _minAge = value;
+    notifyListeners();
+  }
+
+  int get maxDistance => _maxDistance;
+
+  set maxDistance(int value) {
+    _maxDistance = value;
+    notifyListeners();
+  }
+
+  int get minDistance => _minDistance;
+
+  set minDistance(int value) {
+    _minDistance = value;
+    notifyListeners();
+  }
+
+  List<YiddishProficiency> get yiddishProficiencySelection => _yiddishProficiencySelection;
+
+  set yiddishProficiencySelection(List<YiddishProficiency> value) {
+    _yiddishProficiencySelection = value;
+    notifyListeners();
+  }
 }
