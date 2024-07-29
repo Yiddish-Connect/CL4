d600eccfeb24ee09279dc141519ecf61ddba695d
zhzh <nonox530042@gmail.com>
2024-07-29 18:11:46 -0400

    Built the layout of the event list

--- lib/screens/event/eventPage.dart
+++ lib/screens/event/eventPage.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:yiddishconnect/models/yiddishlandEvent.dart';
+import 'package:yiddishconnect/services/firestore.dart';
 
 class EventPage extends StatefulWidget {
   const EventPage({super.key});
@@ -16,8 +19,9 @@ class _EventPageState extends State<EventPage> {
   @override
   Widget build(BuildContext context) {
     print("eventPage build()");
-    return CustomScrollView(
-      slivers: <Widget>[
+    return NestedScrollView(
+      // Header (a sliver search bar + yiddishland banner)
+      headerSliverBuilder: (BuildContext context, bool innerBoxScrolled) => <Widget>[
         // The floating search bar
         SliverAppBar( // https://api.flutter.dev/flutter/material/SliverAppBar-class.html
           pinned: false,
@@ -31,7 +35,7 @@ class _EventPageState extends State<EventPage> {
               children: [
                 Align(
                   alignment: Alignment.center,
-                  child: Image(image: AssetImage("yiddishland.png"))
+                  child: Image(image: AssetImage("assets/yiddishland.png"))
                 ),
                 Align(
                   alignment: Alignment.centerLeft,
@@ -81,18 +85,9 @@ class _EventPageState extends State<EventPage> {
             ),
           ),
         ),
-        // The list of events (With pictures and buttons)
-        SliverList(
-          delegate: SliverChildBuilderDelegate(
-                (BuildContext context, int index) {
-              return ListTile(
-                title: Text('Item #$index'),
-              );
-            },
-            childCount: 100, // Number of items in the list
-          ),
-        ),
       ],
+      // Body (the events)
+      body: _createEventList()
     );
   }
 
@@ -100,5 +95,71 @@ class _EventPageState extends State<EventPage> {
   void dispose() {
     super.dispose();
     searchController.dispose();
+  }
+
+  Widget _createEventList() {
+    return FutureBuilder(
+      future: FirestoreService().yiddishlandEvents.get(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> querySnapshot) {
+        if (querySnapshot.hasError) {
+          print(querySnapshot.error);
+          return Text('Something went wrong');
+        }
+
+        if (querySnapshot.connectionState == ConnectionState.waiting) {
+          return Text("Loading");
+        }
+
+        return ListView(
+          children: querySnapshot.data!.docs.map((DocumentSnapshot document) {
+            YiddishlandEvent event = YiddishlandEvent.fromMap(document.data()! as Map<String, dynamic>, document.id);
+            return Card(
+              margin: EdgeInsets.all(8.0),
+              child: ListTile(
+                contentPadding: EdgeInsets.all(16.0),
+                leading: event.imageSrc.isNotEmpty
+                    ? Image.network(
+                  event.imageSrc,
+                  width: 100,
+                  height: 100,
+                  fit: BoxFit.cover,
+                )
+                    : null,
+                title: Text(
+                  event.title,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 8.0),
+                    Text(
+                      event.description,
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 8.0),
+                    event.src.isNotEmpty
+                        ? GestureDetector(
+                      onTap: () {
+                        launchUrl(mode: LaunchMode.externalApplication, Uri.parse(event.src)); // url_launcher package
+                      },
+                      child: Text(
+                        event.src,
+                        style: TextStyle(
+                          color: Colors.blue,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    )
+                        : Container(),
+                  ],
+                ),
+              ),
+            );
+          }).toList(),
+        );
+
+      }
+    );
   }
 }
