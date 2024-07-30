913346e3e99aa57adf3c1266a94769be5ca6db14
zhzh <nonox530042@gmail.com>
2024-07-30 16:21:43 -0400

    Used flutter_staggered_list to make the event list more fancy.

--- lib/screens/event/eventPage.dart
+++ lib/screens/event/eventPage.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:yiddishconnect/models/yiddishlandEvent.dart';
 import 'package:yiddishconnect/services/firestore.dart';
@@ -19,6 +19,7 @@ class _EventPageState extends State<EventPage> {
 
   @override
   void initState() {
+    // The events data is only fetched from Firestore once here, when the EventPage widget is first built.
     futureEvents = FirestoreService().yiddishlandEvents.get()
       .then((QuerySnapshot snapshot) {
         print("Successfully fetched ${snapshot.size} yiddishland events!");
@@ -122,56 +123,79 @@ class _EventPageState extends State<EventPage> {
           return Text("Loading");
         }
 
-        return ListView(
-          children: querySnapshot.data!.docs.map((DocumentSnapshot document) {
-            YiddishlandEvent event = YiddishlandEvent.fromMap(document.data()! as Map<String, dynamic>, document.id);
-            return Card(
-              margin: EdgeInsets.all(8.0),
-              child: ListTile(
-                contentPadding: EdgeInsets.all(16.0),
-                leading: event.imageSrc.isNotEmpty
-                    ? Image.network(
-                  event.imageSrc,
-                  width: 100,
-                  height: 100,
-                  fit: BoxFit.cover,
+        // return ListView(
+        //   children: querySnapshot.data!.docs.map((DocumentSnapshot document) {
+        //     YiddishlandEvent event = YiddishlandEvent.fromMap(document.data()! as Map<String, dynamic>, document.id);
+        //     return _createEventCard(event);
+        //   }).toList(),
+        // );
+
+        return AnimationLimiter(
+          child: ListView.builder(
+            itemCount: querySnapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              DocumentSnapshot documentSnapshot = querySnapshot.data!.docs[index];
+              YiddishlandEvent event = YiddishlandEvent.fromMap(documentSnapshot.data()! as Map<String, dynamic>, documentSnapshot.id);
+
+              return AnimationConfiguration.staggeredList(
+                position: index,
+                duration: const Duration(milliseconds: 2000), // The list will "float" to the screen. The duration is currently 2000ms.
+                child: SlideAnimation(
+                  verticalOffset: 150.0,
+                  child: FadeInAnimation(child: _createEventCard(event)),
                 )
-                    : null,
-                title: Text(
-                  event.title,
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                ),
-                subtitle: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height: 8.0),
-                    Text(
-                      event.description,
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    SizedBox(height: 8.0),
-                    event.src.isNotEmpty
-                        ? GestureDetector(
-                      onTap: () {
-                        launchUrl(mode: LaunchMode.externalApplication, Uri.parse(event.src)); // url_launcher package
-                      },
-                      child: Text(
-                        event.src,
-                        style: TextStyle(
-                          color: Colors.blue,
-                          decoration: TextDecoration.underline,
-                        ),
-                      ),
-                    )
-                        : Container(),
-                  ],
+              );
+            },
+          ),
+        );
+      }
+    );
+  }
+
+  // Change here to modify the layout of each event card!
+  Widget _createEventCard(YiddishlandEvent event) {
+    return Card(
+      margin: EdgeInsets.all(8.0),
+      child: ListTile(
+        contentPadding: EdgeInsets.all(16.0),
+        leading: event.imageSrc.isNotEmpty
+            ? Image.network(
+          event.imageSrc,
+          width: 100,
+          height: 100,
+          fit: BoxFit.cover,
+        )
+            : null,
+        title: Text(
+          event.title,
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+        ),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 8.0),
+            Text(
+              event.description,
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 8.0),
+            event.src.isNotEmpty
+                ? GestureDetector(
+              onTap: () {
+                launchUrl(mode: LaunchMode.externalApplication, Uri.parse(event.src)); // url_launcher package
+              },
+              child: Text(
+                event.src,
+                style: TextStyle(
+                  color: Colors.blue,
+                  decoration: TextDecoration.underline,
                 ),
               ),
-            );
-          }).toList(),
-        );
-
-      }
+            )
+                : Container(),
+          ],
+        ),
+      ),
     );
   }
 }
