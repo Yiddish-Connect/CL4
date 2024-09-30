c3d47dcc039cc271efde022ecfd3f630d751c785
Leo <hodat@mail.gvsu.edu>
2024-09-30 11:14:11 -0400

    adding the friends page feature, adding the go back button for the chat screen, and friends page button that lead to the message

--- lib/ui/home/friend/friend.dart
+++ lib/ui/home/friend/friend.dart
@@ -1,4 +1,7 @@
+// lib/ui/home/friend/friend.dart
 import 'package:flutter/material.dart';
+import 'friendTitle.dart';
+import '../chat/chat.dart';
 
 class FriendPage extends StatefulWidget {
   const FriendPage({super.key});
@@ -8,19 +11,99 @@ class FriendPage extends StatefulWidget {
 }
 
 class _FriendPageState extends State<FriendPage> {
+  final List<Map<String, String>> friends = [
+    {'id': '1', 'name': 'Leo', 'imageUrl': 'https://example.com/leo.jpg'},
+    {'id': '2', 'name': 'Bie', 'imageUrl': 'https://example.com/bie.jpg'},
+    {'id': '3', 'name': 'Bie1', 'imageUrl': 'https://example.com/bie.jpg'},
+    {'id': '4', 'name': 'Bie2', 'imageUrl': 'https://example.com/bie.jpg'},
+    {'id': '5', 'name': 'Bie3', 'imageUrl': 'https://example.com/bie.jpg'},
+    // Add more friends here
+  ];
+
+  TextEditingController _searchController = TextEditingController();
+  FocusNode _searchFocusNode = FocusNode();
+  String _searchText = "";
+
   @override
   void initState() {
-    print("FriendPage initState()... (This should only happen once)");
+    _searchController.addListener(() {
+      setState(() {
+        _searchText = _searchController.text;
+      });
+    });
     super.initState();
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.brown,
-      child: Center(
-        child: Text('TODO: FriendPage'),
+  void dispose() {
+    _searchController.dispose();
+    _searchFocusNode.dispose();
+    super.dispose();
+  }
+
+  void _navigateToChat(String friendId, String friendName) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => ChatPage(chatUser: friendName),
       ),
-    );;
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List filteredFriends = friends.where((friend) {
+      return friend['name']!.toLowerCase().contains(_searchText.toLowerCase());
+    }).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Friends'),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: _searchController,
+                decoration: InputDecoration(
+                  labelText: 'Search',
+                  hintText: 'Search friends...',
+                  prefixIcon: Icon(Icons.search),
+                ),
+              ),
+              SizedBox(height: 10),
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 200,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    childAspectRatio: 1,
+                  ),
+                  itemCount: filteredFriends.length,
+                  itemBuilder: (context, index) {
+                    return FriendTile(
+                      name: filteredFriends[index]['name']!,
+                      imageUrl: filteredFriends[index]['imageUrl']!,
+                      onTap: () {
+                        _navigateToChat(
+                          filteredFriends[index]['id']!,
+                          filteredFriends[index]['name']!,
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
