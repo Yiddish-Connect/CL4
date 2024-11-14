d33ac08826ecf6f4ba976740f93f349da4a09fdc
Leo <hodat@mail.gvsu.edu>
2024-11-14 17:50:56 -0500

    modified firebase version

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -1,20 +1,25 @@
-importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-messaging-compat.js");
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
 firebase.initializeApp({
-  apiKey: 'AIzaSyBA6cONKgiSYXLaVSU0pABzO-vPQ5_4zX0',
-  appId: '1:447187951174:web:87f8416583c6a578f9b447',
-  messagingSenderId: '447187951174',
-  projectId: 'ydapp-830fe',
-  //authDomain: 'xxx',
-  //databaseURL:'xxx',
-  //storageBucket: 'xxx',
-  //measurementId: 'xxx',
+  apiKey: "AIzaSyCYg35X-5ocVDYFNSZuH5IoR_NVeUIBcuc",
+  databaseURL: "ydapp-830fe.firebaseio.com",
+  authDomain: "ydapp-830fe.firebaseapp.com",
+  projectId: "ydapp-830fe",
+  storageBucket: "ydapp-830fe.appspot.com",
+  messagingSenderId: "447187951174",
+  appId: "1:447187951174:web:87f8416583c6a578f9b447"
 });
-// Necessary to receive background messages:
+
 const messaging = firebase.messaging();
 
-// Optional:
-// messaging.onBackgroundMessage((m) => {
-//   console.log("onBackgroundMessage", m);
-// });
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png'
+  };
+
+  return self.registration.showNotification(notificationTitle, notificationOptions);
+});
