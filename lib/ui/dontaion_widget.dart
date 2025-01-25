fad1928c1e12932ca9a5e3c0d1fe23289265f703
Thatonerookiedev <shatoriag01@gmail.com>
2025-01-25 13:07:43 -0600

    Weeb dosen't work right now but android works totally fine

--- lib/ui/dontaion_widget.dart
+++ lib/ui/dontaion_widget.dart
@@ -1,24 +1,13 @@
 //shatoria
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-var html = '''
-  <html>
-    <body style="margin: 0; padding: 0; overflow: hidden;">
-      <iframe 
-        src="https://nowpayments.io/embeds/donation-widget?api_key=YWW7YS9-A114J08-N1A0YG2-AWGQX2P"
-        width="100%"
-        height="100%"
-        frameborder="0"
-        scrolling="no"
-        style="overflow-y: hidden;"
-      >
-      Can't load widget
-      </iframe>
-    </body>
-  </html>
-''';
+var donationUrl =
+    'https://nowpayments.io/embeds/donation-widget?api_key=YWW7YS9-A114J08-N1A0YG2-AWGQX2P';
 
+//donation screen dosen't work on web
+//see https://pub.dev/packages/webview_flutter form more details
 class DontationScreen extends StatefulWidget {
   const DontationScreen({super.key});
 
@@ -35,7 +24,7 @@ class _DontationScreenState extends State<DontationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const DonationWidget(),
+      body: !kIsWeb ? const DonationWidget() : Placeholder(),
     );
   }
 }
@@ -46,7 +35,7 @@ class DonationWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return WebView(
-      initialUrl: 'https://www.reddit.com/',
+      initialUrl: donationUrl,
       javascriptMode: JavascriptMode.unrestricted,
     );
   }
